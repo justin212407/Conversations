767838f939f1a8258c41640a86611c9dde495ca3
justin212407 <charlesjustin2124@gmail.com>
2026-05-22 14:38:29 +0530

    feat: add turborepo
    Signed-off-by: justin212407 <charlesjustin2124@gmail.com>

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@workspace/ui/components/button"
 import { add } from "@workspace/math/add"
+import { Input } from "@workspace/ui/components/input"
 
 export default function Page() {
   return (
@@ -7,12 +8,13 @@ export default function Page() {
       <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
         <div>
           <h1 className="font-medium">Project ready!</h1>
-          <p>Hello apps/web.</p>
+          <p>Hello apps/widget</p>
           <Button className="mt-2">Button</Button>
         </div>
         <div className="text-muted-foreground font-mono text-xs">
           (Press <kbd>d</kbd> to toggle dark mode)
-          <p>{add(2,2)}</p>
+          {add(1,1)}
+          <Input />
         </div>
       </div>
     </div>
